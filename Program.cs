08f7b971bc946a2f640e59806685d75adf34e459
sorza <alexandre.sorza@outlook.com>
2026-06-07 23:40:53 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using OllamaSharp;
+
+const string agentMarkdownPath = "Agents/code_reviewer.md";
+const string codePath = "Agents/code.md";
+const string ollamaUrl = "http://localhost:11434";
+const string model = "qwen2.5-coder:7b";
+
+var handler = new HttpClientHandler();
+var httpClient = new HttpClient(handler)
+{
+    BaseAddress = new Uri(ollamaUrl),
+    Timeout = TimeSpan.FromMinutes(2)
+};
+
+var ollama = new OllamaApiClient(httpClient);
+var instructions = File.ReadAllText(agentMarkdownPath);
+var code = File.ReadAllText(codePath);
+
+var codeReviewerAgent = ollama
+    .AsAIAgent(new ChatClientAgentOptions
+    {
+        Name = "CodeReviewerAgent",
+        Description = "Agente especista em revisão de código C# .NET",
+        ChatOptions = new ChatOptions
+        {
+            ModelId = model,
+            Temperature = 0.7f,
+            Instructions = instructions
+        }
+    });
+
