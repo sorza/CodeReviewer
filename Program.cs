a298bf6ec65b4b13ad3e67a35ef49ddb0c92a23f
sorza <alexandre.sorza@outlook.com>
2026-07-12 21:05:32 -0300

    feat: Compartilhando contexto entre modelos

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.AI;
 using OllamaSharp;
 using OpenAI.Chat;
 using OpenAI.Responses;
+using System.ComponentModel;
 
 const string agentMarkdownPath = "Agents/code_reviewer.md";
 const string codePath = "Agents/code.txt";
@@ -13,7 +14,7 @@ var handler = new HttpClientHandler();
 var httpClient = new HttpClient(handler)
 {
     BaseAddress = new Uri(ollamaUrl),
-    Timeout = TimeSpan.FromMinutes(2)
+    Timeout = TimeSpan.FromMinutes(5)
 };
 
 var ollama = new OllamaApiClient(httpClient);
@@ -32,15 +33,24 @@ var codeReviewerAgent = ollama.AsAIAgent(new ChatClientAgentOptions
     }
 });
 
-var response = await codeReviewerAgent.RunAsync<Result>(code);
-
-Console.WriteLine($"Pontuação geral: {response.Result.OverallScore}/100");
-Console.WriteLine($"Nota: {response.Result.Grade}");
-Console.WriteLine($"Resumo: {response.Result.Summary}");
-
-sealed record Result
+var codeReviewerHelper = ollama.AsAIAgent(new ChatClientAgentOptions
 {
-    public string OverallScore { get; set; } = string.Empty;
-    public string Grade { get; set; } = string.Empty;
-    public string Summary { get; set; } = string.Empty;
-}
+    Name = "CodeReviewerHelper",
+    Description = "Agente especialista em auxiliar os revisores de código.",
+    ChatOptions = new ChatOptions
+    {
+        ModelId = model,
+        Temperature = 0.7f,
+        Instructions = "Sempre responda com o nome do revisor."
+    }
+});
+
+
+var session = await codeReviewerAgent.CreateSessionAsync();
+
+var prompt = $"Olá, eu sou o revisor Alexandre Zordan, revise o código C# e forneça feedback construtivo, sugestões de melhoria e identifique possíveis bugs ou problemas no código a seguir:\n\n{code}";
+var review  = await codeReviewerAgent.RunAsync(prompt, session);
+Console.WriteLine(review);
+
+var response = await codeReviewerHelper.RunAsync("Qual é o nome do revisor?", session);
+Console.WriteLine(response);
